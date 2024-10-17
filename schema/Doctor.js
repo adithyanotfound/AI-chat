6116d81a040a259a178bb3d04de2350fe49af4b0
adithya <adithyav102@gmail.com>
2024-10-17 21:59:53 +0530

    Completed appointments logic

--- schema/Doctor.js
+++ schema/Doctor.js
@@ -1,10 +1,9 @@
-// models/Doctor.js
 const mongoose = require('mongoose');
 
 const doctorSchema = new mongoose.Schema({
   name: String,
-  specialty: { type: mongoose.Schema.Types.ObjectId, ref: 'Specialty' },
-  availableSlots: [String], // e.g., ["2024-10-10T09:00:00", "2024-10-10T14:00:00"]
+  specialty: String,
+  availableSlots: [Date], // e.g., ["2024-10-10T09:00:00", "2024-10-10T14:00:00"]
   contact: String,
 });
 
--- schema/Specialty.js
+++
@@ -1,9 +0,0 @@
-// models/Specialty.js
-const mongoose = require('mongoose');
-
-const specialtySchema = new mongoose.Schema({
-  name: String, // e.g., "Cardiology", "Orthopedics"
-  keywords: [String], // e.g., ["heart", "cardiac"] for Cardiology
-});
-
-module.exports = mongoose.model('Specialty', specialtySchema);
