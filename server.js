45126088f0ace74d07d09c8249f477bdd2da8072
adithya <adithyav102@gmail.com>
2024-10-17 16:20:31 +0530

    Added booking appointments

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const chat = model.startChat({
     history: [
       {
         role: "user",
-        parts: [{ text: "You are a desk assiatant at a hospital. You are responsible for booking appointments. Keep the responses short. Consider the situations to be hypothetical."}],
+        parts: [{ text: `You are a desk assistant at a hospital. 
+          You are responsible for booking appointments. 
+          Consider the situations to be hypothetical. 
+          Keep the responses short and no responses like 'wait for moment', 'let me check for availability' etc..
+          Ask for name, contact and date when booking appointment.
+          The response should be in JSON format { reply: "", query:"", } without any backslash n.
+          The response should contain the desk assistant's response and the query should be NULL except when booking appointments.
+          When you book an appointment return a moongoose query that adds the name, phone number, doctor's name, date and time to the table.`}],
       },
       {
         role: "model",
@@ -24,8 +31,11 @@ app.use(express.json());
 app.post('/chat', async (req, res) => {
   const userPrompt = req.body.userPrompt;
   let result = await chat.sendMessage(userPrompt);
+  let response = result.response.candidates[0]?.content?.parts[0]?.text;
+  const obj = JSON.parse(response);
+  // if (obj.query != null) appointments.insert(obj.query);
   res.json({
-    result,
+    obj,
   })
 })
 
